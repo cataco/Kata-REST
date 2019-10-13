67f96cbd07396ba12c62a8cbee66fc20b41f868a
Catalina <cata.cordoba8@gmail.com>
2019-10-13 16:10:07 -0500

    new app

--- portafolio/portafolio/settings.py
+++ portafolio/portafolio/settings.py
@@ -73,12 +73,14 @@ WSGI_APPLICATION = 'portafolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'portafolio',
+    'USER': 'postgres',
+    'PASSWORD': 'kta890908',
+    'HOST': 'localhost',
+    'PORT': '',
+}}
 
 
 # Password validation

335e81d4c9526fca691c30a1085e2cb2ecbe3d81
Catalina <cata.cordoba8@gmail.com>
2019-10-13 16:44:30 -0500

    test portafolios estado 200 - rojo

--- portafolio/kataportafolio/tests.py
+++ portafolio/kataportafolio/tests.py
@@ -1,3 +1,10 @@
+import self as self
 from django.test import TestCase
 
-# Create your tests here.
+
+class PortafolioTestCase(TestCase):
+
+    def test_lista_portafolio_estado(self):
+        url = '/portafolio/'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, 200)
